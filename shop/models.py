0ae61f7869ec054f6974e379bc72f587c0b18ee2
Chris Moynan <Chrismoynan@outlook.com>
2024-10-18 14:22:22 +0000

    removed shop templates/models and views due to chnage in how I want view to look

--- shop/models.py
+++ shop/models.py
@@ -1,20 +1,4 @@
 from django.db import models
 
 # Create your models here.
-class Product(models.Model):
-    CATEGORY_CHOICES = [
-        ('clothing', 'Clothing'),
-        ('electronics', 'Electronics'),
-        ('workout', 'Workout Plan'),
-        ('meal', 'Meal Plan'),
-    ]
 
-    name = models.CharField(max_length=100)
-    description = models.TextField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES)
-    image = models.ImageField(upload_to='products/')
-    available = models.BooleanField(default=True)
-
-    def __str__(self):
-        return self.name
--- shop/views.py
+++ shop/views.py
@@ -1,19 +1,2 @@
 from django.shortcuts import render
-from .models import Product
 
-# Create your views here.
-
-
-
-# View to display the product list by category
-def product_list(request, category=None):
-    if category:
-        products = Product.objects.filter(category=category, available=True)
-    else:
-        products = Product.objects.filter(available=True)
-    return render(request, 'shop/product_list.html', {'products': products, 'category': category})
-
-# View to display product details
-def product_detail(request, pk):
-    product = Product.objects.get(pk=pk)
-    return render(request, 'shop/product_detail.html', {'product': product})
