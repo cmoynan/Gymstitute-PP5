1009eeff0c006caa54152c4d3ca314d85c0a754e
Chris Moynan <Chrismoynan@outlook.com>
2024-10-18 22:54:27 +0000

    added link to categories and updated admin site with category

--- gymstitute_pp5/settings.py
+++ gymstitute_pp5/settings.py
@@ -107,7 +107,7 @@ WSGI_APPLICATION = 'gymstitute_pp5.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
     }
 }
 
