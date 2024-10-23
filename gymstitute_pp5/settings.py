f6fdd20a7cd23eb57eef478f852de5b0eb9c6f25
Chris Moynan <Chrismoynan@outlook.com>
2024-10-23 18:52:21 +0000

    tried adding bag app but caused too many issues. deleted and just add some more nagistion options for users

--- gymstitute_pp5/settings.py
+++ gymstitute_pp5/settings.py
@@ -76,6 +76,7 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
                 'django.template.context_processors.media',
 
+
             ],
         },
     },
